085e8f5c6f36d21e73fc074177f7a4e110e253b3
manami-project <13179375+manami-project@users.noreply.github.com>
2024-07-18 19:53:22 +0200

    Added access to dataset files

---
+++ lib/src/main/kotlin/io/github/manamiproject/modb/app/dataset/DefaultDatasetFileAccessor.kt
@@ -0,0 +1,62 @@
+package io.github.manamiproject.modb.app.dataset
+
+import io.github.manamiproject.modb.app.config.AppConfig
+import io.github.manamiproject.modb.app.config.Config
+import io.github.manamiproject.modb.app.dataset.DatasetFileType.*
+import io.github.manamiproject.modb.core.extensions.RegularFile
+import io.github.manamiproject.modb.core.extensions.createZipOf
+import io.github.manamiproject.modb.core.extensions.writeToFile
+import io.github.manamiproject.modb.core.logging.LoggerDelegate
+import io.github.manamiproject.modb.serde.json.*
+import io.github.manamiproject.modb.core.models.Anime
+import io.github.manamiproject.modb.serde.json.models.Dataset
+
+/**
+ * Handles the access to the dataset files like
+ * + anime-offline-database.json
+ * + anime-offline-database-minified.json
+ * + anime-offline-database.zip
+ * @since 1.0.0
+ * @property appConfig Application specific configuration. Uses [AppConfig] by default.
+ * @property deserializer Deserializes the anime dataset file.
+ * @property jsonSerializer Writes the anime dataset file.
+ */
+class DefaultDatasetFileAccessor(
+    private val appConfig: Config = AppConfig.instance,
+    private val deserializer: ExternalResourceJsonDeserializer<Dataset> = DefaultExternalResourceJsonDeserializer(deserializer = AnimeListJsonStringDeserializer.instance),
+    private val jsonSerializer: JsonSerializer<Collection<Anime>> = AnimeListJsonSerializer(clock = appConfig.clock()),
+): DatasetFileAccessor {
+
+    override suspend fun fetchEntries(): List<Anime> {
+        log.info { "Loading database" }
+
+        return deserializer.deserialize(offlineDatabaseFile(JSON_MINIFIED)).data
+    }
+
+    override suspend fun saveEntries(anime: List<Anime>) {
+        log.info { "Writing json to file." }
+        jsonSerializer.serialize(anime, minify = false).writeToFile(offlineDatabaseFile(JSON))
+
+        log.info { "Creating minified json." }
+        jsonSerializer.serialize(anime, minify = true).writeToFile(offlineDatabaseFile(JSON_MINIFIED))
+
+        log.info { "Creating zip file." }
+        offlineDatabaseFile(ZIP).createZipOf(offlineDatabaseFile(JSON_MINIFIED))
+    }
+
+    override fun offlineDatabaseFile(type: DatasetFileType): RegularFile = when (type) {
+        JSON -> appConfig.outputDirectory().resolve("anime-offline-database.json")
+        JSON_MINIFIED -> appConfig.outputDirectory().resolve("anime-offline-database-minified.json")
+        ZIP -> appConfig.outputDirectory().resolve("anime-offline-database.zip")
+    }
+
+    companion object {
+        private val log by LoggerDelegate()
+
+        /**
+         * Singleton of [DefaultDatasetFileAccessor]
+         * @since 1.0.0
+         */
+        val instance = DefaultDatasetFileAccessor()
+    }
+}
---
+++ lib/src/test/kotlin/io/github/manamiproject/modb/app/dataset/DefaultDatasetFileAccessorTest.kt
@@ -0,0 +1,249 @@
+package io.github.manamiproject.modb.app.dataset
+
+import io.github.manamiproject.modb.app.TestAppConfig
+import io.github.manamiproject.modb.app.TestExternalResourceJsonDeserializerDataset
+import io.github.manamiproject.modb.app.TestJsonSerializerCollectionAnime
+import io.github.manamiproject.modb.app.config.Config
+import io.github.manamiproject.modb.app.dataset.DatasetFileType.*
+import io.github.manamiproject.modb.core.extensions.Directory
+import io.github.manamiproject.modb.core.extensions.RegularFile
+import io.github.manamiproject.modb.core.models.Anime
+import io.github.manamiproject.modb.serde.json.ExternalResourceJsonDeserializer
+import io.github.manamiproject.modb.serde.json.JsonSerializer
+import io.github.manamiproject.modb.serde.json.models.Dataset
+import io.github.manamiproject.modb.test.tempDirectory
+import kotlinx.coroutines.runBlocking
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
+import kotlin.io.path.Path
+import kotlin.test.Test
+
+internal class DefaultDatasetFileAccessorTest {
+
+    @Nested
+    inner class SaveEntriesTests {
+
+        @Test
+        fun `creates three files using serializers`() {
+            tempDirectory {
+                // given
+                val testAppConfig = object: Config by TestAppConfig {
+                    override fun outputDirectory(): Directory = tempDir
+                }
+
+                var hasBeenInvoked = false
+                val testAnimeListJsonSerializer = object: JsonSerializer<Collection<Anime>> by TestJsonSerializerCollectionAnime {
+                    override suspend fun serialize(obj: Collection<Anime>, minify: Boolean): String {
+                        hasBeenInvoked = true
+                        return "{}"
+                    }
+                }
+
+                val databaseAccess = DefaultDatasetFileAccessor(
+                    appConfig = testAppConfig,
+                    deserializer = TestExternalResourceJsonDeserializerDataset,
+                    jsonSerializer = testAnimeListJsonSerializer,
+                )
+
+                // when
+                databaseAccess.saveEntries(emptyList())
+
+                // then
+                assertThat(hasBeenInvoked).isTrue()
+                assertThat(tempDir.resolve("anime-offline-database.json")).exists()
+                assertThat(tempDir.resolve("anime-offline-database-minified.json")).exists()
+                assertThat(tempDir.resolve("anime-offline-database.zip")).exists()
+            }
+        }
+    }
+
+    @Nested
+    inner class FetchEntriesTests {
+
+        @Test
+        fun `delegates loading to the deserializer`() {
+            runBlocking {
+                // given
+                val testAppConfig = object: Config by TestAppConfig {
+                    override fun outputDirectory(): Directory = Path(".")
+                }
+
+                var hasBeenInvoked = false
+                val testExternalResourceAvroDeserializer = object: ExternalResourceJsonDeserializer<Dataset> by TestExternalResourceJsonDeserializerDataset {
+                    override suspend fun deserialize(file: RegularFile): Dataset {
+                        hasBeenInvoked = true
+                        return Dataset(
+                            lastUpdate = "2020-01-01",
+                            data = emptyList(),
+                        )
+                    }
+                }
+
+                val databaseAccess = DefaultDatasetFileAccessor(
+                    appConfig = testAppConfig,
+                    deserializer = testExternalResourceAvroDeserializer,
+                    jsonSerializer = TestJsonSerializerCollectionAnime,
+                )
+
+                // when
+                databaseAccess.fetchEntries()
+
+                // then
+                assertThat(hasBeenInvoked).isTrue()
+            }
+        }
+    }
+
+    @Nested
+    inner class OfflineDatabaseFileTests {
+
+        @Test
+        fun `json database file resides in offlineDatabaseDirectory`() {
+            tempDirectory {
+                // given
+                val testAppConfig = object: Config by TestAppConfig {
+                    override fun outputDirectory(): Directory = tempDir
+                }
+
+                val databaseAccess = DefaultDatasetFileAccessor(
+                    appConfig = testAppConfig,
+                    deserializer = TestExternalResourceJsonDeserializerDataset,
+                    jsonSerializer = TestJsonSerializerCollectionAnime,
+                )
+
+                // when
+                val parent = databaseAccess.offlineDatabaseFile(JSON).parent
+
+                // then
+                assertThat(parent).isEqualTo(testAppConfig.outputDirectory())
+            }
+        }
+
+        @Test
+        fun `minified json database file resides in offlineDatabaseDirectory`() {
+            tempDirectory {
+                // given
+                val testAppConfig = object: Config by TestAppConfig {
+                    override fun outputDirectory(): Directory = tempDir
+                }
+
+                val databaseAccess = DefaultDatasetFileAccessor(
+                    appConfig = testAppConfig,
+                    deserializer = TestExternalResourceJsonDeserializerDataset,
+                    jsonSerializer = TestJsonSerializerCollectionAnime,
+                )
+
+                // when
+                val parent = databaseAccess.offlineDatabaseFile(JSON_MINIFIED).parent
+
+                // then
+                assertThat(parent).isEqualTo(testAppConfig.outputDirectory())
+            }
+        }
+
+        @Test
+        fun `zipped database file resides in offlineDatabaseDirectory`() {
+            tempDirectory {
+                // given
+                val testAppConfig = object: Config by TestAppConfig {
+                    override fun outputDirectory(): Directory = tempDir
+                }
+
+                val databaseAccess = DefaultDatasetFileAccessor(
+                    appConfig = testAppConfig,
+                    deserializer = TestExternalResourceJsonDeserializerDataset,
+                    jsonSerializer = TestJsonSerializerCollectionAnime,
+                )
+
+                // when
+                val parent = databaseAccess.offlineDatabaseFile(ZIP).parent
+
+                // then
+                assertThat(parent).isEqualTo(testAppConfig.outputDirectory())
+            }
+        }
+
+        @Test
+        fun `return correct database file based on type JSON`() {
+            tempDirectory {
+                // given
+                val testAppConfig = object: Config by TestAppConfig {
+                    override fun outputDirectory(): Directory = tempDir
+                }
+
+                val databaseAccess = DefaultDatasetFileAccessor(
+                    appConfig = testAppConfig,
+                    deserializer = TestExternalResourceJsonDeserializerDataset,
+                    jsonSerializer = TestJsonSerializerCollectionAnime,
+                )
+
+                // when
+                val result = databaseAccess.offlineDatabaseFile(JSON).fileName.toString()
+
+                // then
+                assertThat(result).isEqualTo("anime-offline-database.json")
+            }
+        }
+
+        @Test
+        fun `return correct database file based on type JSON_MINIFIED`() {
+            tempDirectory {
+                // given
+                val testAppConfig = object: Config by TestAppConfig {
+                    override fun outputDirectory(): Directory = tempDir
+                }
+
+                val databaseAccess = DefaultDatasetFileAccessor(
+                    appConfig = testAppConfig,
+                    deserializer = TestExternalResourceJsonDeserializerDataset,
+                    jsonSerializer = TestJsonSerializerCollectionAnime,
+                )
+
+                // when
+                val result = databaseAccess.offlineDatabaseFile(JSON_MINIFIED).fileName.toString()
+
+                // then
+                assertThat(result).isEqualTo("anime-offline-database-minified.json")
+            }
+        }
+
+        @Test
+        fun `return correct database file based on type ZIP`() {
+            tempDirectory {
+                // given
+                val testAppConfig = object: Config by TestAppConfig {
+                    override fun outputDirectory(): Directory = tempDir
+                }
+
+                val databaseAccess = DefaultDatasetFileAccessor(
+                    appConfig = testAppConfig,
+                    deserializer = TestExternalResourceJsonDeserializerDataset,
+                    jsonSerializer = TestJsonSerializerCollectionAnime,
+                )
+
+                // when
+                val result = databaseAccess.offlineDatabaseFile(ZIP).fileName.toString()
+
+                // then
+                assertThat(result).isEqualTo("anime-offline-database.zip")
+            }
+        }
+    }
+
+    @Nested
+    inner class CompanionObjectTests {
+
+        @Test
+        fun `instance property always returns same instance`() {
+            // given
+            val previous = DefaultDatasetFileAccessor.instance
+
+            // when
+            val result = DefaultDatasetFileAccessor.instance
+
+            // then
+            assertThat(result).isExactlyInstanceOf(DefaultDatasetFileAccessor::class.java)
+            assertThat(result===previous).isTrue()
+        }
+    }
+}
