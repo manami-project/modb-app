f7d9deaaacea2ae025ca90e79dc262f9fe951894
manami-project <13179375+manami-project@users.noreply.github.com>
2024-08-28 11:16:54 +0200

    Added UnknownRelatedAnimeRemover

---
+++ lib/src/main/kotlin/io/github/manamiproject/modb/app/relatedanime/DefaultUnknownRelatedAnimeRemover.kt
@@ -0,0 +1,35 @@
+package io.github.manamiproject.modb.app.relatedanime
+
+import io.github.manamiproject.modb.core.logging.LoggerDelegate
+import io.github.manamiproject.modb.core.models.Anime
+
+/**
+ * This implementation doesn't use dead entries to check if entries need to be removed, but checks the data within the
+ * dataset. A related anime is only acceptable if a corresponding entry exists in any `sources` of any anime in the
+ * dataset.
+ * @since 1.0.0
+ */
+class DefaultUnknownRelatedAnimeRemover: UnknownRelatedAnimeRemover {
+
+    override fun removeRelatedAnimeWhichHaveNoCorrespondingEntryInSources(allEntries: List<Anime>): List<Anime> {
+        log.info { "Removing related anime which are unknown." }
+
+        val allSources = allEntries.flatMap { it.sources }.toHashSet()
+
+        allEntries.map { anime ->
+            anime.removeRelatedAnimeIf { uri -> !allSources.contains(uri) }
+        }
+
+        return allEntries
+    }
+
+    companion object {
+        private val log by LoggerDelegate()
+
+        /**
+         * Singleton of [DefaultUnknownRelatedAnimeRemover]
+         * @since 1.0.0
+         */
+        val instance: DefaultUnknownRelatedAnimeRemover by lazy { DefaultUnknownRelatedAnimeRemover() }
+    }
+}
