7b7c5299cdf16309aa8b7b2dc9f8cbe21bccfdbf
manami-project <13179375+manami-project@users.noreply.github.com>
2025-01-31 20:46:23 +0100

    Added and fixed KDoc

--- simkl/src/main/kotlin/io/github/manamiproject/modb/animecountdown/AnimeCountdownConfig.kt
+++ simkl/src/main/kotlin/io/github/manamiproject/modb/animecountdown/AnimeCountdownConfig.kt
@@ -6,6 +6,10 @@ import io.github.manamiproject.modb.core.config.Hostname
 import io.github.manamiproject.modb.core.config.MetaDataProviderConfig
 import java.net.URI
 
+/**
+ * Configuration for downloading and converting anime data from animecountdown.com
+ * @since 1.0.0
+ */
 public object AnimeCountdownConfig: MetaDataProviderConfig {
 
     override fun fileSuffix(): FileSuffix  = "html"
