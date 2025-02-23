85b198cbfcbe73b56d61a64477e29de1228048e7
manami-project <13179375+manami-project@users.noreply.github.com>
2025-02-23 10:55:51 +0100

    chore: Check if junit config improves build runs

--- livechart/build.gradle.kts
+++ livechart/build.gradle.kts
@@ -46,6 +46,8 @@ tasks.withType<Test> {
     reports.html.required.set(false)
     reports.junitXml.required.set(true)
     maxParallelForks = rootProject.extra["maxParallelForks"] as Int
+    systemProperty("junit.jupiter.execution.parallel.enabled", "true")
+    systemProperty("junit.jupiter.execution.parallel.mode.default", "concurrent")
 }
 
 val sourcesJar by tasks.registering(Jar::class) {
