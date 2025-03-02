37a2baf2709233087a774685b734612a3c197b4a
manami-project <13179375+manami-project@users.noreply.github.com>
2025-03-02 22:11:44 +0100

    Added additional test for ignoring dead entries

--- lib/src/test/kotlin/io/github/manamiproject/modb/app/crawlers/notify/NotifyDatasetDownloadCrawlerTest.kt
+++ lib/src/test/kotlin/io/github/manamiproject/modb/app/crawlers/notify/NotifyDatasetDownloadCrawlerTest.kt
@@ -214,6 +214,96 @@ internal class NotifyDatasetDownloadCrawlerTest {
     }
 
     @Test
+    fun `correctly ignore entries which only have an ID`() {
+        // given
+        tempDirectory {
+            val testNotifyConfig = object: MetaDataProviderConfig by TestMetaDataProviderConfig {
+                override fun hostname(): Hostname = NotifyAnimeDatasetDownloaderConfig.hostname()
+                override fun buildDataDownloadLink(id: String): URI = NotifyAnimeDatasetDownloaderConfig.buildDataDownloadLink(id)
+                override fun fileSuffix(): FileSuffix = NotifyAnimeDatasetDownloaderConfig.fileSuffix()
+            }
+
+            val testNotifyRelationsConfig = object: MetaDataProviderConfig by TestMetaDataProviderConfig {
+                override fun hostname(): Hostname = NotifyRelationsDatasetDownloaderConfig.hostname()
+                override fun buildDataDownloadLink(id: String): URI = NotifyRelationsDatasetDownloaderConfig.buildDataDownloadLink(id)
+                override fun fileSuffix(): FileSuffix = NotifyRelationsDatasetDownloaderConfig.fileSuffix()
+            }
+
+            val testWorkingDir = tempDir.resolve("notify.moe").createDirectory()
+            val testRelationsWorkingDir = tempDir.resolve("notify.moe-relations").createDirectory()
+            val testAppConfig = object: Config by TestAppConfig {
+                override fun workingDir(metaDataProviderConfig: MetaDataProviderConfig): Directory = when (metaDataProviderConfig.buildDataDownloadLink()) {
+                    NotifyAnimeDatasetDownloaderConfig.buildDataDownloadLink() -> testWorkingDir
+                    NotifyRelationsDatasetDownloaderConfig.buildDataDownloadLink() -> testRelationsWorkingDir
+                    else -> shouldNotBeInvoked()
+                }
+            }
+
+            val testHttpClient = object: HttpClient by TestHttpClient {
+                override suspend fun get(url: URL, headers: Map<String, Collection<String>>): HttpResponse {
+                    val body: ByteArray = when (url) {
+                        NotifyAnimeDatasetDownloaderConfig.buildDataDownloadLink().toURL() -> loadTestResource("crawler/notify/NotifyDatasetDownloadCrawlerTest/ignore-dead-entries.txt")
+                        NotifyRelationsDatasetDownloaderConfig.buildDataDownloadLink().toURL() -> loadTestResource("crawler/notify/NotifyDatasetDownloadCrawlerTest/example-relations-dataset.txt")
+                        else -> shouldNotBeInvoked()
+                    }
+
+                    return HttpResponse(
+                        code = 200,
+                        body = body,
+                    )
+                }
+            }
+
+            val dcsDir = tempDir.resolve("dcs").createDirectory()
+            val testDownloadControlStateAccessor = object: DownloadControlStateAccessor by TestDownloadControlStateAccessor {
+                override fun downloadControlStateDirectory(metaDataProviderConfig: MetaDataProviderConfig): Directory = dcsDir
+            }
+
+            val crawler = NotifyDatasetDownloadCrawler(
+                appConfig = testAppConfig,
+                metaDataProviderConfig = testNotifyConfig,
+                relationsMetaDataProviderConfig = testNotifyRelationsConfig,
+                httpClient = testHttpClient,
+                downloadControlStateAccessor = testDownloadControlStateAccessor,
+                deadEntriesAccessor = TestDeadEntriesAccessor,
+            )
+
+            val expectedFiles = setOf(
+                testWorkingDir.resolve("iEOy6VGHg.${testNotifyConfig.fileSuffix()}"),
+                testWorkingDir.resolve("_0f7tKmig.${testNotifyConfig.fileSuffix()}"),
+                testWorkingDir.resolve("0kLGhFimR.${testNotifyConfig.fileSuffix()}"),
+                testWorkingDir.resolve("1vie5FmmR.${testNotifyConfig.fileSuffix()}"),
+            )
+
+            val expectedRelationsFiles = setOf(
+                testRelationsWorkingDir.resolve("aL4F2FimR.${testNotifyRelationsConfig.fileSuffix()}"),
+                testRelationsWorkingDir.resolve("0NixcKimg.${testNotifyRelationsConfig.fileSuffix()}"),
+                testRelationsWorkingDir.resolve("s6dGL1FVR.${testNotifyRelationsConfig.fileSuffix()}"),
+                testRelationsWorkingDir.resolve("FZEIkXeSg.${testNotifyRelationsConfig.fileSuffix()}"),
+            )
+
+            // when
+            crawler.start()
+
+            //then
+            expectedFiles.forEach {
+                assertThat(it).exists()
+                val content = it.readFile()
+                assertThat(content).startsWith("{")
+                assertThat(content).contains(it.fileName().remove(".${testNotifyConfig.fileSuffix()}"))
+                assertThat(content).endsWith("}")
+            }
+            expectedRelationsFiles.forEach {
+                assertThat(it).exists()
+                val content = it.readFile()
+                assertThat(content).startsWith("{")
+                assertThat(content).contains(it.fileName().remove(".${testNotifyRelationsConfig.fileSuffix()}"))
+                assertThat(content).endsWith("}")
+            }
+        }
+    }
+
+    @Test
     fun `correctly identify dead entry`() {
         // given
         tempDirectory {
