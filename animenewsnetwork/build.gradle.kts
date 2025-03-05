0cfec0ec386db5ecb7b2aee1f50ae16ce80e9f98
manami-project <13179375+manami-project@users.noreply.github.com>
2025-03-05 21:16:42 +0100

    Added AnimenewsnetworkConfig

--- animenewsnetwork/build.gradle.kts
+++ animenewsnetwork/build.gradle.kts
@@ -10,55 +10,25 @@ plugins {
 group = "io.github.manamiproject"
 version = project.findProperty("release.version") as String? ?: ""
 
+val projectName = "modb-app"
+val moduleName = "modb-animenewsnetwork"
 val githubUsername = "manami-project"
 
 repositories {
     mavenCentral()
-    maven {
-        name = "kommand"
-        url = uri("https://maven.pkg.github.com/$githubUsername/kommand")
-        credentials {
-            username = parameter("GH_USERNAME", githubUsername)
-            password = parameter("GH_PACKAGES_READ_TOKEN")
-        }
-    }
 }
 
 dependencies {
     api(libs.kotlin.stdlib)
-    api(project(":anidb"))
-    api(project(":anilist"))
-    api(project(":anime-planet"))
-    api(project(":animenewsnetwork"))
-    api(project(":anisearch"))
     api(project(":core"))
-    api(project(":kitsu"))
-    api(project(":livechart"))
-    api(project(":myanimelist"))
-    api(project(":notify"))
-    api(project(":serde"))
-    api(project(":simkl"))
 
-    implementation(libs.kommand)
-    implementation(libs.logback.classic)
-    implementation(libs.commons.text)
-
-    testImplementation(libs.kotlin.reflect)
+    testImplementation(libs.logback.classic)
     testImplementation(project(":test"))
 }
 
 kotlin {
+    explicitApi()
     jvmToolchain(JavaVersion.VERSION_21.toString().toInt())
-}
-
-kover {
-    reports {
-        filters {
-            excludes {
-                annotatedBy("io.github.manamiproject.modb.core.coverage.KoverIgnore")
-            }
-        }
-    }
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
@@ -86,6 +56,50 @@ val sourcesJar by tasks.registering(Jar::class) {
 val javaDoc by tasks.registering(Jar::class) {
     archiveClassifier.set("javadoc")
     from(sourceSets.main.get().allSource)
+}
+
+publishing {
+    repositories {
+        maven {
+            name = moduleName
+            url = uri("https://maven.pkg.github.com/$githubUsername/$projectName")
+            credentials {
+                username = parameter("GH_USERNAME", githubUsername)
+                password = parameter("GH_PACKAGES_RELEASE_TOKEN")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = moduleName
+            version = project.version.toString()
+
+            from(components["java"])
+            artifact(sourcesJar.get())
+            artifact(javaDoc.get())
+
+            pom {
+                packaging = "jar"
+                name.set(moduleName)
+                description.set("This lib contains downloader and converter for downloading raw data from animenewsnetwork.com and convert it to an anime object.")
+                url.set("https://github.com/$githubUsername/$projectName")
+
+                licenses {
+                    license {
+                        name.set("AGPL-V3")
+                        url.set("https://www.gnu.org/licenses/agpl-3.0.txt")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git@github.com:$githubUsername/$projectName.git")
+                    developerConnection.set("scm:git:ssh://github.com:$githubUsername/$projectName.git")
+                    url.set("https://github.com/$githubUsername/$projectName")
+                }
+            }
+        }
+    }
 }
 
 fun parameter(name: String, default: String = ""): String {
