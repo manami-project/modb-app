844b092afe3d1af8f25e955ce183fb77f88b14c5
manami-project <13179375+manami-project@users.noreply.github.com>
2025-05-28 17:51:03 +0200

    mal: Added extraction for studios and producers

--- anisearch/src/test/kotlin/io/github/manamiproject/modb/anisearch/UpdateTestResources.kt
+++ anisearch/src/test/kotlin/io/github/manamiproject/modb/anisearch/UpdateTestResources.kt
@@ -131,7 +131,7 @@ internal fun main(): Unit = runCoroutine {
         delay(random(5000, 10000))
     }
 
-    print("Done")
+    println("Done")
 }
 
 private fun resourceFile(file: String): Path {
