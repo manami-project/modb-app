c353ac664b8faaddd4b01daff8910cf96dd5e031
manami-project <13179375+manami-project@users.noreply.github.com>
2025-06-01 11:07:06 +0200

    kitsu: In case meta.count is 0 onDeadEntry should be invoked

--- kitsu/src/main/kotlin/io/github/manamiproject/modb/kitsu/KitsuDownloader.kt
+++ kitsu/src/main/kotlin/io/github/manamiproject/modb/kitsu/KitsuDownloader.kt
@@ -6,6 +6,8 @@ import io.github.manamiproject.modb.core.coroutines.ModbDispatchers.LIMITED_NETW
 import io.github.manamiproject.modb.core.downloader.Downloader
 import io.github.manamiproject.modb.core.extensions.EMPTY
 import io.github.manamiproject.modb.core.extensions.neitherNullNorBlank
+import io.github.manamiproject.modb.core.extractor.DataExtractor
+import io.github.manamiproject.modb.core.extractor.JsonDataExtractor
 import io.github.manamiproject.modb.core.httpclient.DefaultHttpClient
 import io.github.manamiproject.modb.core.httpclient.HttpClient
 import io.github.manamiproject.modb.core.httpclient.HttpResponseRetryCase
@@ -23,6 +25,7 @@ public class KitsuDownloader(
     private val httpClient: HttpClient = DefaultHttpClient(isTestContext = metaDataProviderConfig.isTestContext()).apply {
         retryBehavior.addCases(HttpResponseRetryCase { it.code == 400 })
     },
+    private val extractor: DataExtractor = JsonDataExtractor,
 ) : Downloader {
 
     override suspend fun download(id: AnimeId, onDeadEntry: suspend (AnimeId) -> Unit): String = withContext(LIMITED_NETWORK) {
@@ -35,7 +38,23 @@ public class KitsuDownloader(
         check(response.bodyAsText.neitherNullNorBlank()) { "Response body was blank for [kitsuId=$id] with response code [${response.code}]" }
 
         return@withContext when(response.code) {
-            200 -> response.bodyAsText
+            200 -> {
+                val data = extractor.extract(response.bodyAsText, mapOf(
+                    "entries" to "$.meta.count",
+                ))
+                val entries = data.intOrDefault("entries")
+                when (entries) {
+                    0 -> {
+                        onDeadEntry.invoke(id)
+                        EMPTY
+                    }
+                    1 -> {
+                        response.bodyAsText
+                    }
+                    else -> throw IllegalStateException("Anime with id [${id}] returned [$entries] entries.")
+                }
+                response.bodyAsText
+            }
             404 -> {
                 onDeadEntry.invoke(id)
                 EMPTY
