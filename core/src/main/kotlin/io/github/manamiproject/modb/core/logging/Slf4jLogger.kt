97f40d5c85d2a6b940a46400e5a3d4edec1c4661
manami-project <13179375+manami-project@users.noreply.github.com>
2025-06-26 23:02:00 +0200

    Added slf4j-jul-to-slf4j bridge which allows logs from java.util.logging to be picked up by ModbLogger which uses SLF4j underneath
    This is important for OkHttpClient, because those logs were shown in stdout, but haven't been written to the log files.

--- core/src/main/kotlin/io/github/manamiproject/modb/core/logging/Slf4jLogger.kt
+++ core/src/main/kotlin/io/github/manamiproject/modb/core/logging/Slf4jLogger.kt
@@ -1,12 +1,17 @@
 package io.github.manamiproject.modb.core.logging
 
 import org.slf4j.LoggerFactory
+import org.slf4j.bridge.SLF4JBridgeHandler
 import kotlin.reflect.KClass
 
 internal class Slf4jLogger(
     ref: KClass<*>,
     private val slf4jLogger: org.slf4j.Logger = LoggerFactory.getLogger(ref.java),
 ): Logger {
+
+    init {
+        SLF4JBridgeHandler.install()
+    }
 
     override fun error(message: () -> String) = slf4jLogger.error(message.invoke())
 
