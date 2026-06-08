cf5c5717a8da985f5d9aacb012caf158fab4348d
manami-project <13179375+manami-project@users.noreply.github.com>
2026-06-08 19:12:45 +0200

    anidb: Changed data link url

--- anidb/src/main/kotlin/io/github/manamiproject/modb/anidb/AnidbConfig.kt
+++ anidb/src/main/kotlin/io/github/manamiproject/modb/anidb/AnidbConfig.kt
@@ -3,6 +3,7 @@ package io.github.manamiproject.modb.anidb
 import io.github.manamiproject.modb.core.config.FileSuffix
 import io.github.manamiproject.modb.core.config.Hostname
 import io.github.manamiproject.modb.core.config.MetaDataProviderConfig
+import java.net.URI
 
 /**
  * Configuration for downloading and converting anime data from anidb.net
@@ -12,5 +13,7 @@ public object AnidbConfig : MetaDataProviderConfig {
 
     override fun hostname(): Hostname = "anidb.net"
 
-    override fun fileSuffix(): FileSuffix = "html"
+    override fun fileSuffix(): FileSuffix = "xml"
+
+    override fun buildDataDownloadLink(id: String): URI = URI("http://api.${hostname()}:9001/httpapi?request=anime&client=mediabrowser&clientver=1&protover=1&aid=$id")
 }
